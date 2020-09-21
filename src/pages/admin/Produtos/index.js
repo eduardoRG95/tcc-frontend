a0195f8b587052787e0d0706793a116c594e8caf
eduardoRG95 <eduardoraupp7@gmail.com>
2020-09-20 21:15:34 -0300

    ajustado front

--- src/pages/admin/Produtos/index.js
+++ src/pages/admin/Produtos/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from '../Components/Admin/Header';
 import List from '../Components/Admin/List';
+
+import api from '../../../services/api';
 
 import { Container } from 'react-bootstrap';
 
@@ -9,8 +11,17 @@ import { Container } from 'react-bootstrap';
 import './styles.css';
 
 export default function Produtos() {
-    function ListarProdutos () {
 
+    ListarProdutos();
+
+    async function ListarProdutos () {
+        try {
+            const response = await api.get('produto');
+            console.log(response);
+            return response;
+        }catch(error){
+            alert('Falha ao carregar produtos!')
+        }
     }
     return (
         <div className="produtos-container">

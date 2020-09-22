e83b1ef1fee2000872da1c704aeb21bd20977479
eduardoRG95 <eduardoraupp7@gmail.com>
2020-09-22 19:11:55 -0300

    list

--- src/pages/admin/Components/Admin/List/index.js
+++ src/pages/admin/Components/Admin/List/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import { Row, Col, ListGroup } from 'react-bootstrap';
 
+
 export default function List(props) {
-    console.log(props.listProdutos);
+
+    const [listagem, setListagem] = useState(props.listProdutos);
+    
+    const itens = listagem.map((item) =>
+            <ListGroup.Item key={item.id} className="item-list-usuarios">{item.nome}</ListGroup.Item>
+        );
+        
     return (
         <Row className="justify-content-md-center">
             <Col md={10}>
-                <ListGroup variant="flush"  className="list-usuarios">
-                    <ListGroup.Item action  className="item-list-usuarios">Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item action  className="item-list-usuarios">Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item action  className="item-list-usuarios">Morbi leo risus</ListGroup.Item>
-                    <ListGroup.Item action  className="item-list-usuarios">Porta ac consectetur ac</ListGroup.Item>
+                <ListGroup variant="flush" className="list-usuarios">
+                    {itens}
                 </ListGroup>
             </Col>
         </Row>
